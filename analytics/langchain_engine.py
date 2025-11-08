05561c32a23d34f8455931065076c7f544827077
hummingbirdtestai <hummingbirdtestai@gmail.com>
2025-11-08 21:39:37 +0530

    Update langchain_engine.py

--- analytics/langchain_engine.py
+++ analytics/langchain_engine.py
@@ -1,10 +1,15 @@
 import os
 from langchain_openai import ChatOpenAI
 from langchain_community.utilities import SQLDatabase
-from langchain.chains import SQLDatabaseSequentialChain
+from langchain_community.chains.sql_database import SQLDatabaseSequentialChain
 
-DB_URL = os.getenv("DATABASE_URL")  # from Railway env
+DB_URL = os.getenv("DATABASE_URL")  # Supabase connection string
 db = SQLDatabase.from_uri(DB_URL)
 
 llm = ChatOpenAI(model="gpt-4-turbo", temperature=0)
-analytics_chain = SQLDatabaseSequentialChain.from_llm(llm, db, verbose=True)
+
+analytics_chain = SQLDatabaseSequentialChain.from_llm(
+    llm,
+    db,
+    verbose=True
+)
